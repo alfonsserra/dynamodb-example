5795689a2a94f33eec73f1104f7062678f0e868e
alfonsserra <alfonsoserra@gmail.com>
2018-06-20 21:21:27 +0200

    Add CRUD with JSON attribute

--- src/main/java/com/systelab/dynamodb/DynamoDBCRUDManager.java
+++ src/main/java/com/systelab/dynamodb/DynamoDBCRUDManager.java
@@ -1,10 +1,14 @@
 package com.systelab.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-
-import java.util.Arrays;
-import java.util.HashSet;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 
 public class DynamoDBCRUDManager extends DynamoDBManager {
 
@@ -12,29 +16,97 @@ public class DynamoDBCRUDManager extends DynamoDBManager {
         super(isLocal);
     }
 
-    public void createItem(String tableName) {
+    public void createItem(String tableName, String json) {
 
         Table table = dynamoDB.getTable(tableName);
         try {
-
             Item item = new Item().withPrimaryKey("Id", 120)
-                    .withString("Title", "Book 120 Title")
-                    .withString("ISBN", "120-1111111111")
-                    .withStringSet("Authors", new HashSet<String>(Arrays.asList("Author12", "Author22")))
-                    .withNumber("Price", 20).withString("Dimensions", "8.5x11.0x.75").withNumber("PageCount", 500)
-                    .withBoolean("InPublication", false).withString("ProductCategory", "Book");
+                    .withJSON("Representation", json);
             table.putItem(item);
-
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("Create items failed.");
             System.err.println(e.getMessage());
+        }
+    }
 
+    public void retrieveItem(String tableName, Object id) {
+        Table table = dynamoDB.getTable(tableName);
+
+        try {
+            Item item = table.getItem("Id", id, "Representation", null);
+
+            System.out.println("Printing item after retrieving it....");
+            System.out.println(item.getJSON("Representation"));
+        } catch (Exception e) {
+            System.err.println("GetItem failed.");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void updateAddNewAttribute(String tableName, Object id, String json) {
+        Table table = dynamoDB.getTable(tableName);
+
+        try {
+
+            UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("Id", id)
+                    .withUpdateExpression("set #na = :val1").withNameMap(new NameMap().with("#na", "Representation"))
+                    .withValueMap(new ValueMap().withJSON(":val1", json)).withReturnValues(ReturnValue.ALL_NEW);
+
+            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+
+            // Check the response.
+            System.out.println("Printing item after adding new attribute...");
+            System.out.println(outcome.getItem().getJSON("Representation"));
+
+        } catch (Exception e) {
+            System.err.println("Failed to add new attribute in " + tableName);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void deleteItem(String tableName, Object id) {
+
+        Table table = dynamoDB.getTable(tableName);
+
+        try {
+
+            DeleteItemSpec deleteItemSpec = new DeleteItemSpec().withPrimaryKey("Id", id).withReturnValues(ReturnValue.ALL_OLD);
+
+            DeleteItemOutcome outcome = table.deleteItem(deleteItemSpec);
+
+            // Check the response.
+            System.out.println("Printing item that was deleted...");
+            System.out.println(outcome.getItem().getJSON("Representation"));
+
+        } catch (Exception e) {
+            System.err.println("Error deleting item in " + tableName);
+            System.err.println(e.getMessage());
         }
     }
 
     public static void main(String[] args) {
         DynamoDBCRUDManager crudManager = new DynamoDBCRUDManager(true);
-        crudManager.createItem("ProductCatalog");
+        String json = "{\n" +
+                "    \"name\":\"John\",\n" +
+                "    \"age\":30,\n" +
+                "    \"cars\": [\n" +
+                "        { \"name\":\"Ford\", \"models\":[ \"Fiesta\", \"Focus\", \"Mustang\" ] },\n" +
+                "        { \"name\":\"BMW\", \"models\":[ \"320\", \"X3\", \"X5\" ] },\n" +
+                "        { \"name\":\"Fiat\", \"models\":[ \"500\", \"Panda\" ] }\n" +
+                "    ]\n" +
+                " }";
+
+        String json1 = "{\n" +
+                "\"name\":\"John\",\n" +
+                "\"age\":30,\n" +
+                "\"cars\":[ \"Ford\", \"BMW\", \"Fiat\" ]\n" +
+                "}";
+
+        crudManager.createItem("ProductCatalog", json);
+        crudManager.retrieveItem("ProductCatalog", 120);
+        crudManager.updateAddNewAttribute("ProductCatalog", 120, json1);
+        crudManager.retrieveItem("ProductCatalog", 120);
+
+        crudManager.deleteItem("ProductCatalog", 120);
     }
 }
